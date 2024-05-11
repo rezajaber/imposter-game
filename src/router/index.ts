8fd154f122bf6b9adf664720326f8a5431584a75
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-12 01:08:50 +0200

    create paths and views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,38 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+
+import LandingScreen from "@/views/LandingScreen.vue";
+import LobbyScreen from "@/views/LobbyScreen.vue";
+import GameScreen from "@/views/GameScreen.vue";
+import RoleScreen from "@/views/RoleScreen.vue";
+import ResultScreen from "@/views/ResultScreen.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      name: "landing",
+      component: LandingScreen,
+    },
+    {
+      path: "/lobby",
+      name: "lobby",
+      component: LobbyScreen,
+    },
+    {
+      path: "/role",
+      name: "role",
+      component: GameScreen,
+    },
+    {
+      path: "/game",
+      name: "game",
+      component: RoleScreen,
+    },
+    {
+      path: "/result",
+      name: "result",
+      component: ResultScreen,
     },
   ],
 });
