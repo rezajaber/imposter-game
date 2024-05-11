2cf9e4826f8304b87674b3b0e2c1a3b39353361e
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-12 01:02:47 +0200

    fundament

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueDevTools from "vite-plugin-vue-devtools";
+
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [vue(), VueDevTools()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
