0595dfe5dab14bc6b504c6c7ae6d2b6bbefead9f
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-12 03:30:26 +0200

    create secret agent role screen

--- src/views/RoleScreen.vue
+++ src/views/RoleScreen.vue
@@ -4,25 +4,44 @@ import DynamicHeadings from "@/components/DynamicHeadings.vue";
 
 <template>
   <div class="flex flex-col items-center gap-4">
-    <DynamicHeadings mainTitle="YOU ARE AN" role="IMPOSTER" />
+    <!-- 
+    <DynamicHeadings mainTitle="YOU ARE AN..." role="IMPOSTER" />
     <img
       src="../../src/assets/img/imposter.png"
       class="w-96 animate-pulse"
       alt=""
     />
-    <p class="suffix-shadow font-creepster text-3xl tracking-wider text-white">
+    
+    <p class="suffix-shadow-imposter font-creepster text-3xl tracking-wider text-white">
       WIN BY FOOLING THE OTHERS
     </p>
+  --></div>
 
-    <!-- <DynamicHeadings mainTitle="YOU ARE AN" />
-    <img src="../../src/assets/img/agent.png" class="w-96" alt="" />
-    <DynamicHeadings suffixColor="text-green-500" role="SECRET AGENT" />
-    -->
+  <div class="flex flex-col items-center gap-12">
+    <DynamicHeadings
+      mainTitle="YOU WORD IS..."
+      suffixColor="text-green-500"
+      role="GENSHIN IMPACT"
+    />
+    <img
+      src="../../src/assets/img/genshin-impact.webp"
+      class="w-[40rem] rounded-3xl"
+      alt=""
+    />
+    <p
+      class="suffix-shadow-agent font-creepster text-3xl tracking-wider text-white"
+    >
+      WIN BY VOTING OUT THE IMPOSTERS
+    </p>
   </div>
 </template>
 
 <style scoped>
-.suffix-shadow {
+.suffix-shadow-imposter {
   text-shadow: 4px 4px #a10c0c;
+}
+
+.suffix-shadow-agent {
+  text-shadow: 4px 4px #22c55e;
 }
 </style>
