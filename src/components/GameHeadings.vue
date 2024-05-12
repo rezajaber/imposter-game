47dbb47d7ee1553e8c2ce72516f2539ed1cfec2f
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-12 15:22:13 +0200

    LandingScreen - Responsive - Check

--- src/components/GameHeadings.vue
+++ src/components/GameHeadings.vue
@@ -17,13 +17,17 @@ const props = defineProps({
 
 <template>
   <div class="grid gap-1 text-center">
-    <h1 class="title-shadow font-creepster text-8xl tracking-wider text-white">
+    <h1
+      class="title-shadow font-creepster text-5xl tracking-wider text-white sm:text-8xl"
+    >
       {{ props.mainTitle }}
-      <span class="suffix-shadow text-primary">{{
+      <span class="suffix-shadow text-7xl text-primary sm:text-8xl">{{
         props.mainTitleSuffix
       }}</span>
     </h1>
-    <h3 class="title-shadow font-creepster text-3xl tracking-wider text-white">
+    <h3
+      class="title-shadow font-creepster text-2xl tracking-wider text-white sm:text-3xl"
+    >
       {{ props.subtitle }}
     </h3>
   </div>
